1005e7af0f540dc464bc64f27ea9d3d96a49fa2d
Joonhee Jeong <jeonggoo75@gmail.com>
2023-06-27 15:47:00 +0900

    test #17 - selectAll of TeamDao

--- src/test/java/dao/TeamDaoTest.java
+++ src/test/java/dao/TeamDaoTest.java
@@ -1,6 +1,7 @@
 package dao;
 
 import domain.Team;
+import lombok.extern.log4j.Log4j2;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -10,9 +11,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Log4j2
 public class TeamDaoTest {
 
     static TeamDao teamDao;
@@ -41,5 +44,21 @@ public class TeamDaoTest {
 //                        .anyMatch(name -> name.equals(teamName)));
 
         teamDao.rollback();
+    }
+
+    @Test
+    void selectAll() {
+        List<Team> teamList = teamDao.selectAll();
+        log.info(teamList);
+
+        assertTrue(teamList.stream()
+                .map(Team::getName)
+                .anyMatch(name -> name.equals("한화 이글스")));
+        assertTrue(teamList.stream()
+                .map(Team::getName)
+                .anyMatch(name -> name.equals("두산 베어스")));
+        assertTrue(teamList.stream()
+                .map(Team::getName)
+                .anyMatch(name -> name.equals("롯데 자이언츠")));
     }
 }
