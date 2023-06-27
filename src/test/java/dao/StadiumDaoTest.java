051daf5abe15eb833dc19ec58291b4cbe42e00c9
Joonhee Jeong <jeonggoo75@gmail.com>
2023-06-27 14:09:57 +0900

    test #7 - insert(stadium)
    야구장 추가 메소드를 테스트합니다.
      - 테스트가 실제로 반영되지 않도록 트랜잭션을 이용합니다.

--- src/test/java/dao/StadiumDaoTest.java
+++ src/test/java/dao/StadiumDaoTest.java
@@ -1,14 +1,22 @@
 package dao;
 
 import lombok.extern.log4j.Log4j2;
+import model.Stadium;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Log4j2
 public class StadiumDaoTest {
@@ -22,6 +30,24 @@ public class StadiumDaoTest {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         stadiumDao = StadiumDao.getInstance();
         stadiumDao.setSqlSessionFactory(sqlSessionFactory);
+    }
+
+    @AfterEach
+    void afterEach() {
+        stadiumDao.rollback();
+    }
+
+    @Test
+    void insert() {
+        final String nameToFind = "대구 삼성 라이온즈 파크";
+        Stadium stadiumInserted = Stadium.builder()
+                .name(nameToFind)
+                .build();
+        stadiumDao.insert(stadiumInserted, true);
+
+        assertTrue(stadiumDao.selectAll().stream()
+                .map(Stadium::getName)
+                .anyMatch(name -> name.equals(nameToFind)));
     }
 
     @Test
