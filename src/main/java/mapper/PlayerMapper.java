6c76b5532e14fa3c2d02ba197d2f16ac55278dff
Joonhee Jeong <jeonggoo75@gmail.com>
2023-06-28 13:54:11 +0900

    add #23 - selectListByList of PlayerMapper
    - 인터페이스에 해당 메소드 정의
    - 매퍼에 쿼리 등록

--- src/main/java/mapper/PlayerMapper.java
+++ src/main/java/mapper/PlayerMapper.java
@@ -2,6 +2,9 @@ package mapper;
 
 import domain.Player;
 
+import java.util.List;
+
 public interface PlayerMapper {
     void insert(Player player);
+    List<Player> selectListByTeamId(Long teamId);
 }
