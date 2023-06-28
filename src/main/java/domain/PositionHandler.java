a01e63ae76051dc9ce308eb969b2073c09cbe058
Joonhee Jeong <jeonggoo75@gmail.com>
2023-06-28 14:28:29 +0900

    add #23 - PositionHandler
    결과 문자열로 일치하는 Position enum을 찾기 위해 핸들러를 추가합니다.
    - 잘못된 포지션이 입력될 경우 예외를 추가합니다.
    - 매퍼에 resultMap을 등록합니다

---
+++ src/main/java/domain/PositionHandler.java
@@ -0,0 +1,38 @@
+package domain;
+
+import exception.IllegalPositionDescriptionException;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PositionHandler extends BaseTypeHandler<Position> {
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Position parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, parameter.getDescription());
+    }
+
+    @Override
+    public Position getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        String description = rs.getString(columnName);
+        return Position.getByDescription(description)
+                .orElseThrow(() -> new IllegalPositionDescriptionException("잘못된 포지션: " + description));
+    }
+
+    @Override
+    public Position getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        String description = rs.getString(columnIndex);
+        return Position.getByDescription(description)
+                .orElseThrow(() -> new IllegalPositionDescriptionException("잘못된 포지션: " + description));
+    }
+
+    @Override
+    public Position getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String description = cs.getString(columnIndex);
+        return Position.getByDescription(description)
+                .orElseThrow(() -> new IllegalPositionDescriptionException("잘못된 포지션: " + description));
+    }
+}
