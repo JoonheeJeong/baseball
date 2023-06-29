b5545fcdf5620c8f5c3f88ed56fbf0f01d925ee6
JOOWON <wer07032@naver.com>
2023-06-29 15:40:03 +0900

    implement OutplayerService

--- src/main/java/service/OutPlayerService.java
+++ src/main/java/service/OutPlayerService.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 
 @Log4j2
-public class OutPlayerService implements BaseBallService{
+public class OutPlayerService implements BaseBallService {
 
     private static OutPlayerService INSTANCE;
     private final OutPlayerDao outPlayerDao;
@@ -26,14 +26,24 @@ public class OutPlayerService implements BaseBallService{
         }
         return INSTANCE;
     }
+
     @Override
     public void register(HashMap<String, String> map) {
         try {
             outPlayerDao.setSqlSessionFactory(get());
-            OutPlayer.builder()
+            OutPlayer outPlayer = OutPlayer.builder()
                     .playerId(Long.valueOf(map.get("playerId")))
                     .reason(Reason.getByDescription(map.get("reason")))
                     .build();
+            try {
+                outPlayerDao.insert(outPlayer, true);
+                outPlayerDao.commit();
+                outPlayerDao.updateById(Long.valueOf(map.get("playerId")), true);
+                outPlayerDao.commit();
+            } catch (Exception e) {
+                outPlayerDao.rollback();
+                log.warn("트랜잭션 오류");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
