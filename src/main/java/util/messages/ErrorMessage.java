2907ae1a06b2e7e22d06100af122641e17e5008d
JOOWON <wer07032@naver.com>
2023-06-29 19:58:33 +0900

    implement #24 Message interface
    응답 메세지와 예외 메세지를 인터페이스에 상수로 구현했습니다.

---
+++ src/main/java/util/messages/ErrorMessage.java
@@ -0,0 +1,13 @@
+package util.messages;
+
+public interface ErrorMessage {
+    String ERR_MSG_ILLEGAL_REQUEST_TYPE = "알맞은 요청 형식이 아닙니다.";
+    String ERR_MSG_ILLEGAL_PARAMETER = "알맞은 파라미터명이 아닙니다.";
+    String ERR_MSG_ILLEGAL_PARAMETER_TYPE = "알맞은 파라미터 형식이 아닙니다.";
+    String ERR_MSG_ILLEGAL_COMMAND = "알맞은 요청명이 아닙니다.";
+    String ERR_MSG_DUPLICATE_POSITION = "중복된 포지션입니다.";
+    String ERR_MSG_DUPLICATE_PARAMETER = "이미 존재하는 파라미터입니다.";
+    String ERR_MSG_TRANSACTION = "트랜잭션 오류";
+    String ERR_MSG_NO_SUCH = "입력되지 않은 값이 있습니다.";
+}
+
